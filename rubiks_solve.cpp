6e7241603cd126730b3b1c40adf2b47684e93690
Jarom Christensen <jarom77@gmail.com>
2025-10-09 12:50:09 -0400

    Add basic solution

--- rubiks_solve.cpp
+++ rubiks_solve.cpp
@@ -1,13 +1,22 @@
 #include "rubiks.h"
 #include "rubiksIterators.h"
 
+// try each turn to see if it solves the cube. Then give up.
 bool solve_basic(Rubiks& cube, Move *solution) {
-    // solve
-    return false;
+    int index = 0;
+    for (MoveIter i = MoveIter(cube); !i.atEnd(); i++) {
+        if (cube.isSolved()) {
+            solution[index] = i.getMove();
+            index++;
+        }
+    }
+    solution[index] = Move{NO_COLOR};
+    return cube.isSolved();
 }
 
 bool solve(Rubiks& cube, Move *solution, SolveMethod method, size_t max_moves) {
     if (method == Basic)
         return solve_basic(cube, solution);
+    solution[0] = Move{NO_COLOR};
     return false;
 }
